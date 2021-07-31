f51254ba285a0d3185c59e5e1203aa295c17d2ca
Oleg Kovalov <oleg@hey.com>
2021-07-31 13:23:44 +0200

    Add SafeReader and SafeWriter
    Signed-off-by: Oleg Kovalov <oleg@hey.com>

--- io.go
+++ io.go
@@ -3,6 +3,7 @@ package synx
 import (
 	"context"
 	"io"
+	"sync"
 )
 
 type readerFn func(p []byte) (n int, err error)
@@ -24,4 +25,36 @@ func Copy(ctx context.Context, dst io.Writer, src io.Reader) (int64, error) {
 
 	written, err := io.Copy(dst, readerCtx)
 	return written, err
+}
+
+// SafeReader synchronizes concurrent reads to the underlying io.Writer.
+func SafeReader(r io.Reader) io.Reader {
+	return &safeReader{r: r}
+}
+
+type safeReader struct {
+	mu sync.Mutex
+	r  io.Reader
+}
+
+func (sr *safeReader) Read(b []byte) (int, error) {
+	sr.mu.Lock()
+	defer sr.mu.Unlock()
+	return sr.r.Read(b)
+}
+
+// SafeWriter synchronizes concurrent writes to the underlying io.Writer.
+func SafeWriter(w io.Writer) io.Writer {
+	return &safeWriter{w: w}
+}
+
+type safeWriter struct {
+	mu sync.Mutex
+	w  io.Writer
+}
+
+func (sw *safeWriter) Write(b []byte) (int, error) {
+	sw.mu.Lock()
+	defer sw.mu.Unlock()
+	return sw.w.Write(b)
 }
