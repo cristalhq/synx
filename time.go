02bc3d67aa13da415920410b180406172a0fe0db
Oleg Kovalov <oleg@hey.com>
2021-11-05 20:44:37 +0100

    Add SlowStartTimer

--- time.go
+++ time.go
@@ -17,3 +17,43 @@ func Sleep(ctx context.Context, duration time.Duration) bool {
 		return true
 	}
 }
+
+func SlowStartTimer(ctx context.Context, d time.Duration, before ...time.Duration) (<-chan time.Time, context.CancelFunc) {
+	if len(before) == 0 {
+		t := time.NewTimer(d)
+		return t.C, func() { t.Stop() }
+	}
+
+	ch := make(chan time.Time)
+	stop := func() { close(ch) }
+
+	go func() {
+		defer stop()
+
+		t := time.NewTimer(d)
+		for _, b := range before {
+			select {
+			case <-ctx.Done():
+				return
+			case now := <-time.After(b):
+				ch <- now
+			}
+		}
+
+		if !t.Stop() {
+			<-t.C
+		}
+		t.Reset(d)
+		defer t.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case now := <-t.C:
+				ch <- now
+			}
+		}
+	}()
+	return ch, stop
+}
