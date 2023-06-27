800911f2d8f2c84c5db120fdd09de8776adb41e0
Oleg Kovalov <oleg@hey.com>
2023-06-27 19:21:45 +0200

    Revert for older Go

--- map.go
+++ map.go
@@ -46,31 +46,31 @@ func (m *Map[K, V]) Delete(key K) {
 	m.m.Delete(key)
 }
 
-// Swap swaps the value for a key and returns the previous value if any.
-// The loaded result reports whether the key was present.
-func (m *Map[K, V]) Swap(key K, value V) (previous V, loaded bool) {
-	v, ok := m.m.Swap(key, value)
-	if v != nil {
-		previous = v.(V)
-	}
-	return previous, ok
-}
+// // Swap swaps the value for a key and returns the previous value if any.
+// // The loaded result reports whether the key was present.
+// func (m *Map[K, V]) Swap(key K, value V) (previous V, loaded bool) {
+// 	v, ok := m.m.Swap(key, value)
+// 	if v != nil {
+// 		previous = v.(V)
+// 	}
+// 	return previous, ok
+// }
 
-// CompareAndSwap swaps the old and new values for key
-// if the value stored in the map is equal to old.
-// The old value must be of a comparable type.
-func (m *Map[K, V]) CompareAndSwap(key K, old, new V) bool {
-	return m.m.CompareAndSwap(key, old, new)
-}
+// // CompareAndSwap swaps the old and new values for key
+// // if the value stored in the map is equal to old.
+// // The old value must be of a comparable type.
+// func (m *Map[K, V]) CompareAndSwap(key K, old, new V) bool {
+// 	return m.m.CompareAndSwap(key, old, new)
+// }
 
-// CompareAndDelete deletes the entry for key if its value is equal to old.
-// The old value must be of a comparable type.
-//
-// If there is no current value for key in the map, CompareAndDelete
-// returns false (even if the old value is the nil interface value).
-func (m *Map[K, V]) CompareAndDelete(key K, old V) (deleted bool) {
-	return m.m.CompareAndDelete(key, old)
-}
+// // CompareAndDelete deletes the entry for key if its value is equal to old.
+// // The old value must be of a comparable type.
+// //
+// // If there is no current value for key in the map, CompareAndDelete
+// // returns false (even if the old value is the nil interface value).
+// func (m *Map[K, V]) CompareAndDelete(key K, old V) (deleted bool) {
+// 	return m.m.CompareAndDelete(key, old)
+// }
 
 // Range calls f sequentially for each key and value present in the map.
 // If f returns false, range stops the iteration.
